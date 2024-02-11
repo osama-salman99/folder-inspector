0e9454cfaf53c60e3018d0758239dfe6fdad907b
Osama <osama.salman99@gmail.com>
2024-02-11 19:55:58 +0300

    feat: apply threading

--- src/main/java/osmosis/folder/inspector/calculation/DirectorySizeCalculationTask.java
+++ src/main/java/osmosis/folder/inspector/calculation/DirectorySizeCalculationTask.java
@@ -2,6 +2,7 @@ package osmosis.folder.inspector.calculation;
 
 import osmosis.folder.inspector.container.Container;
 import osmosis.folder.inspector.container.DirectoryContainer;
+import osmosis.folder.inspector.container.FileContainer;
 
 import java.util.List;
 import java.util.concurrent.ForkJoinTask;
@@ -17,18 +18,27 @@ public class DirectorySizeCalculationTask extends RecursiveTask<Long> {
     @Override
     protected Long compute() {
         List<Container> childrenContainers = directoryContainer.getChildrenContainers();
-        long size = childrenContainers.stream()
+        long directoriesSizes = calculateDirectoriesSizes(childrenContainers);
+        long filesSizes = calculateFilesSizes(childrenContainers);
+        long size = directoriesSizes + filesSizes;
+        directoryContainer.setSize(size);
+        directoryContainer.invokeListener();
+        return size;
+    }
+
+    private long calculateDirectoriesSizes(List<Container> childrenContainers) {
+        return childrenContainers.stream()
                 .filter(container -> container instanceof DirectoryContainer)
                 .map(container -> new DirectorySizeCalculationTask((DirectoryContainer) container))
                 .map(ForkJoinTask::fork)
                 .mapToLong(ForkJoinTask::join)
                 .sum();
-        size += childrenContainers.stream()
-                .filter(container -> !(container instanceof DirectoryContainer))
+    }
+
+    private static long calculateFilesSizes(List<Container> childrenContainers) {
+        return childrenContainers.stream()
+                .filter(container -> container instanceof FileContainer)
                 .mapToLong(Container::getSize)
                 .sum();
-        directoryContainer.setSize(size);
-        directoryContainer.invokeListener();
-        return size;
     }
 }
