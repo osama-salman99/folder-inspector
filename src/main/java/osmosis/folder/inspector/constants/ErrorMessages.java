2b091c61a830407cff082423a63ffd6fcc9f4b89
Osama <osama.salman99@gmail.com>
2024-03-04 19:16:16 +0300

    feat: grey-out inspect button when path is empty

--- src/main/java/osmosis/folder/inspector/constants/ErrorMessages.java
+++ src/main/java/osmosis/folder/inspector/constants/ErrorMessages.java
@@ -4,6 +4,8 @@ public class ErrorMessages {
     public static final String ERROR_OCCURRED = "Error occurred: ";
     public static final String DIRECTORY_DOES_NOT_EXIST = "Directory does not exist";
     public static final String PATH_DOES_NOT_POINT_TO_A_DIRECTORY = "Path does not point to a directory";
+    public static final String PATH_CANNOT_BE_EMPTY = "Path cannot be empty";
+
     private ErrorMessages() {
     }
 }
